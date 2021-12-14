c06485e79b4d6b2c7223ba741e7851dfc81dbebc
tomas.payer <tomaspayer18@gmail.com>
2021-12-14 16:01:21 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -61,3 +61,26 @@ window.onscroll = () => {
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
+
+$(document).ready(function(){
+    var cartCountValue = 0;
+    var cartCount = $('.cart .count');
+    $(cartCount).text(cartCountValue);
+
+    $('.cart-btn').on('click', function(){
+        var cartBtn = this;
+        var cartCountPosition = $(cartCount).offset();
+        var btnPosition = $(this).offset();
+        var leftPos =
+        cartCountPosition.left < btnPosition.left
+        ? btnPosition.left - (btnPosition.left - cartCountPosition.left)
+        : cartCountPosition.left;
+
+        var topPos = 
+        cartCountPosition.top < btnPosition.top
+        ? cartCountPosition.top
+        : cartCountPosition.top;
+        $(cartBtn)
+        .append("<span class='count'>1</span>");
+    })
+})

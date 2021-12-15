bcc5fdb5da55f35df147a441cec93fe06f8e23bc
Tomás Payer <91624465+TomasPayer@users.noreply.github.com>
2021-12-15 20:17:46 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -31,6 +31,8 @@ form.addEventListener("submit", e => {
 
 })
 
+
+//responsive del los icons del navbar "search" y "carrito" 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
@@ -62,43 +64,43 @@ window.onscroll = () => {
     cartItem.classList.remove('active');
 }
 
-$(document).ready(function(){
+//Animaciones de add to cart
+$(document).ready(function () {
     var cartCountValue = 0;
     var cartCount = $('.cart .count');
     $(cartCount).text(cartCountValue);
 
-    $('.cart-btn').on('click', function(){
+    $('.cart-btn').on('click', function () {
         var cartBtn = this;
         var cartCountPosition = $(cartCount).offset();
         var btnPosition = $(this).offset();
         var leftPos =
-        cartCountPosition.left < btnPosition.left
-        ? btnPosition.left - (btnPosition.left - cartCountPosition.left)
-        : cartCountPosition.left;
+            cartCountPosition.left < btnPosition.left ?
+            btnPosition.left - (btnPosition.left - cartCountPosition.left) :
+            cartCountPosition.left;
 
-        var topPos = 
-        cartCountPosition.top < btnPosition.top
-        ? cartCountPosition.top
-        : cartCountPosition.top;
+        var topPos =
+            cartCountPosition.top < btnPosition.top ?
+            cartCountPosition.top :
+            cartCountPosition.top;
         $(cartBtn)
-        .append("<span class='count'>1</span>");
+            .append("<span class='count'>1</span>");
 
-        $(cartBtn).find(".count").each(function(i, count){
+        $(cartBtn).find(".count").each(function (i, count) {
             $(count).offset({
-                left: leftPos,
-                top: topPos
-            })
-            .animate(
-                {
-opcity:0
-            },
-            800,
-            function(){
-                $(this).remove();
-                cartCountValue++;
-                $(cartCount).text(cartCountValue);
-            }
-            );
+                    left: leftPos,
+                    top: topPos
+                })
+                .animate({
+                        opcity: 0
+                    },
+                    800,
+                    function () {
+                        $(this).remove();
+                        cartCountValue++;
+                        $(cartCount).text(cartCountValue);
+                    }
+                );
         });
     });
 });
